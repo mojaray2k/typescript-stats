cf5827475c1a21e9f01924511b2ce4670a3997f3
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-23 18:29:32 -0400

    created an enum

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,17 @@ const matches = fs
 
 let manUnitedWins = 0;
 
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
