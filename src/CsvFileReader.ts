4e2c740d5a7c0b3bf13717ecec56293227c7a9d7
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-23 20:58:59 -0400

    added generics and an abstract class to CsvFileReader to make it easy to reuse for any csv file

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,13 +1,11 @@
 import fs from 'fs';
-import {dateStringToDate} from './utils';
-import {MatchResult} from './MatchResult';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-export class CsvFileReader {
+export abstract class CsvFileReader<T> {
   // data is a two dimensional array of strings
-  data: MatchData[] = [];
+  data: T[] = [];
 
   constructor(public filename: string) {}
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -18,19 +16,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            // the below is a Type Assertion
-            row[5] as MatchResult,
-            row[6],
-          ];
-        }
-      );
+      .map(this.mapRow);
   }
 }
