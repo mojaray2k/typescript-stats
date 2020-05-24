29208af1629d50b16352fd4897bc37e8953b9937
Amen Ra <amenra@Amen-Ras-MacBook-Pro.local>
2020-05-24 12:01:41 -0400

    implimented more complex composition with generating html reports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {CsvFileReader} from './CsvFileReader';
 import {ConsoleReport} from './reportTargets/ConsoleReport';
 import {WinsAnalysis} from './analyzers/WinsAnalysis';
 import {Summary} from './Summary';
+import {HtmlReport} from './reportTargets/HtmlReport';
 
 // Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
@@ -14,7 +15,8 @@ matchReader.load();
 
 const summary = new Summary(
   new WinsAnalysis('Man United'),
-  new ConsoleReport()
+  // new ConsoleReport()
+  new HtmlReport()
 );
 
 summary.buildAndPrintReport(matchReader.matches);
